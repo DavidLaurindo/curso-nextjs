7a642ff6975ca203162c65e975e6d20251c05ba7
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-01-31 19:03:15 -0300

    aula props

---
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,13 @@
+//Sistema de módulo
+import Titulo from '../../components/Titulo'
+
+export default function usandoTitulo() {
+    return (
+        <div>
+            <Titulo
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e excluir coisas!" />
+        </div>
+    )
+}
+//Importando um componente.
