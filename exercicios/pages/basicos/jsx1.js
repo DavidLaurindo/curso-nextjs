7713660fecb6ef70950419203e1027fd096c6c15
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-01-31 13:57:05 -0300

    primeiro commit

---
+++ exercicios/pages/basicos/jsx1.js
@@ -0,0 +1,9 @@
+//JSX (mistura "html"(na verdade não é html) dentro de um código javascript).
+
+export default function(){
+    return (
+        <div>
+            <h1>Segundo</h1>
+        </div>
+    )
+}
---
+++ exercicios/pages/basicos/jsx2.js
@@ -0,0 +1,6 @@
+export default function(){
+    const conteudo = <div><h1>JSX 02</h1></div>
+    return conteudo
+    //Exemplo de maneira.
+}
+
