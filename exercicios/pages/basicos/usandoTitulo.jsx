d79d9fb248bf0721ecf64598097d1371125e1b51
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-02-01 07:42:26 -0300

    class props

--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -6,7 +6,13 @@ export default function usandoTitulo() {
         <div>
             <Titulo
                 principal="Página de Cadastro"
-                secundario="Incluir, alterar e excluir coisas!" />
+                secundario="Incluir, alterar e excluir coisas!" 
+            />
+             <Titulo
+                principal="Página de login"
+                secundario="Informe email e senha" 
+                pequeno={true}
+            />
         </div>
     )
 }
