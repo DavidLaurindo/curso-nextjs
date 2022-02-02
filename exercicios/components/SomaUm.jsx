743c8b979cada970683297e8af53b80e78fc7519
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-02-02 14:23:01 -0300

    classes css

---
+++ exercicios/components/SomaUm.jsx
@@ -0,0 +1,8 @@
+export default function somaUm(props){
+    return(
+        <div>
+            <h1>{props.numero + 2}</h1>
+        </div>
+    )
+}
+//promps é somente leitura(demostração)
---
+++ exercicios/pages/basicos/propsSomenteLeitura.jsx
@@ -0,0 +1,6 @@
+import SomaUm from "../../components/SomaUm";
+
+export default function propsSomenteLeitura(){
+    return <SomaUm numero={10} />
+}
+//props é somente leitura(demostração)
