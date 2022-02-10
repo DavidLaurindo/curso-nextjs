4bd3252340e93aae29f4091f14d577f6f619ad12
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-02-10 17:17:38 -0300

    repetição 2

---
+++ exercicios/data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1, "ipad", 1800),
+    new Produto(2, "monitor", 2000),
+    new Produto(3, "computador gamer", 8000),
+    new Produto(4, "PS5", 5000),
+]
