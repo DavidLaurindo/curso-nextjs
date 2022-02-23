777941f3aab7079b62794d6d4957d07a0f489bfc
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-02-23 18:47:42 -0300

    logica de mega sena

---
+++ exercicios/functions/mega.js
@@ -0,0 +1,15 @@
+export function mega(quant = 6, numeros = []){
+    if(quant < 6 && quant > 60){
+        throw "Quantidade inválida"
+    }
+    if(numeros.length === quant){
+        return numeros.sort((n1, n2) => n1 - n2)
+    }
+
+    const numeroAleatorio = parseInt(Math.random() * 60) + 1
+    if(!numeros.includes(numeroAleatorio)){
+        return mega(quant, [...numeros, numeroAleatorio])
+    }else{
+        return mega(quant, numeros)
+    }
+}
