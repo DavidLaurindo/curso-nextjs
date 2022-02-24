af4e6a92c6bc8c36c71709cae9924899da225331
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-02-24 19:43:24 -0300

    mini projeto mega-sena

--- exercicios/pages/estado/contador.jsx
+++ exercicios/pages/estado/contador.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import ContadorDisplay from "../../components/ContadorDisplay"
+import NumeroDisplay from "../../components/NumeroDisplay"
 
 export default function contador() {
     const [contador, setContador] = useState(0)
@@ -15,7 +15,7 @@ export default function contador() {
             alignItems: "center"
         }}>
             <h1>Contador</h1>
-            <ContadorDisplay numero={contador} />
+            <NumeroDisplay numero={contador} />
             <div>
                 <button onClick={menos}>-</button>
                 <button onClick={mais}>+</button>
---
+++ exercicios/pages/estado/megasena.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import NumeroDisplay from "../../components/NumeroDisplay"
+import { mega } from "../../functions/mega"
+
+export default function megasena(){
+    const [numeros, setNumeros] = useState(mega())
+
+    function renderNumeros(){
+        return numeros.map(n => <NumeroDisplay />)
+    }
+
+    return (
+        <div>
+            <h1>Mega Sena</h1>
+        </div>
+    )
+}
