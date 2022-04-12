f135760d8ca6905a51d8dbc785a1d44b0286f3d4
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-04-12 19:16:18 -0300

    navegação entre páginas part 1 e part 2

--- next-basico/src/pages/rotas/[codigo]/[nome].jsx
+++ next-basico/src/pages/rotas/[codigo]/[nome].jsx
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-export default function buscar() {
+export default function codigoENome() {
     const router = useRouter()
     const codigo = router.query.codigo
+    const nome = router.query.nome
 
     return (
         <div>
-            <h1>Rotas / {codigo} / Buscar</h1>
+            <h1>Rotas / {codigo} / {nome}</h1>
             <Link href="/rotas">
                 <button>Voltar</button>
             </Link>
--- next-basico/src/pages/rotas/index.jsx
+++ next-basico/src/pages/rotas/index.jsx
@@ -1,7 +1,41 @@
-export default function rotas(){
-    return(
+import Link from "next/link"
+import router, { useRouter } from "next/router"
+
+export default function rotas() {
+
+    function navegacaoParams(){
+        router.push({
+            pathname: "/rotas/params",
+            query: {
+                nome: 'Ana Silva',
+                id: 123
+            }
+        })
+    }
+
+    return (
         <div>
             <h1>Rotas Index</h1>
+            <ul>
+                <Link href="/rotas/params?nome=Lucius&id=86586">
+                    <li>Params</li>
+                </Link>
+                <Link href="/rotas/123/buscar">
+                    <li>Buscar</li>
+                </Link>
+                <Link href="/rotas/123/daniel">
+                    <li>Daniel</li>
+                </Link>
+            </ul>
+            <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+
+                    <button onClick={() => router.push("/rotas/123/buscar")}>Buscar</button>
+
+                    <button onClick={() => router.push("/rotas/123/daniel")}>Daniel</button>
+
+                    <button onClick={navegacaoParams}>Params</button>
+
+            </div>
         </div>
     )
 }
