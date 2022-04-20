76f1b28ebf1d3397c56fe180850b9cfc16805118
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-04-20 19:16:40 -0300

    api part 3

--- next-basico/src/pages/questao.jsx
+++ next-basico/src/pages/questao.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function questao() {
     const [questao, setQuestao] = useState(null)
 
-    fetch('http://localhost:3000/api/questao/123').then(resp => resp.json()).then(json => console.log(json))
+    useEffect(() => {
+        fetch('http://localhost:3000/api/questao/123')
+            .then(resp => resp.json())
+            .then(setQuestao)
+    }, [])
+
+function renderizarRespostas(){
+    if(questao){
+        return questao.respostas.map((resp, i) => {
+            return <li key={i}>{resp}</li>
+        })
+    }else{
+        return false
+    }
+}
+
     return (
         <div>
             <h1>Questão</h1>
+            <div>
+                <span>{questao?.id} - {questao?.enunciado}</span>
+                <ul>
+                    {renderizarRespostas()}
+                </ul>
+            </div>
         </div>
     )
 }
