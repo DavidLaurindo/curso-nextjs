83e75f19851cf5271c68fb8e181859519b0a6d06
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2022-06-09 16:34:43 -0300

    múltiplos parametros

--- next-basico/src/pages/form.jsx
+++ next-basico/src/pages/form.jsx
@@ -5,20 +5,20 @@ export default function form() {
     const [idade, setIdade] = useState("0")
     const [usuarios, setUsuarios] = useState([])
 
-    async function salvarUsuario(){
+    async function salvarUsuario() {
         await fetch('/api/form', {
             method: 'POST',
             body: JSON.stringify({ nome, idade })
         })
-setNome("")
-setIdade(0)
+        setNome("")
+        setIdade(0)
 
         const resp = await fetch('/api/form')
         const usuarios = await resp.json()
         setUsuarios(usuarios)
     }
 
-    function renderizarUsuarios(){
+    function renderizarUsuarios() {
         return usuarios.map((usuario, i) => {
             return <li key={i}>{usuario.nome} tem {usuario.idade} anos</li>
         })
@@ -31,11 +31,11 @@ setIdade(0)
                 onChange={e => setNome(e.target.value)} />
             <input type="number" value={idade}
                 onChange={e => setIdade(+e.target.value)} />
-                <button onClick={salvarUsuario}>Enviar</button>
+            <button onClick={salvarUsuario}>Enviar</button>
 
-                <ul>
-                    {renderizarUsuarios()}
-                </ul>
+            <ul>
+                {renderizarUsuarios()}
+            </ul>
         </div>
     )
 }
