2fb2ec61dd94390e89f2fad1b57ce90326ea71f3
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2023-01-30 09:18:39 -0300

    aula de conteúdo estático 01

---
+++ renderizacao/pages/estatico1.jsx
@@ -0,0 +1,19 @@
+/*import { useEffect, useState } from "react"*/
+
+export default function Estatico1(){
+    
+    /*Conteúdo Dinâmico(tranforma a anotação em conteúdo dinâmico)
+    
+    const [num, setNum] = useState(0)
+
+    useEffect(() => {
+        setNum(Math.random())
+    }, [])*/
+
+    return(
+        <div>
+            <h1>Estático 01</h1>
+            {/*<h2>{num}</h2>*/}
+        </div>
+    )
+}
